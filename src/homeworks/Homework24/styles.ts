10370eaf84f65208d4b234cbd6cf03410cff6e50
Andrii <143963266+andriib77@users.noreply.github.com>
2024-03-02 20:27:47 +0100

    homework 24 jokes

---
+++ src/homeworks/Homework24/styles.ts
@@ -0,0 +1,36 @@
+import styled from "@emotion/styled";
+
+export const Homework24Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  padding: 30px;
+`;
+
+export const JokeCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 600px;
+  min-height: 300px;
+  border: 1px solid black;
+  border-radius: 30px;
+  padding: 30px;
+  gap: 40px;
+  background: #ffa812;
+`;
+
+export const JokeFact = styled.p`
+  font-size: 40px;
+  font-weight: bold;
+  color: black;
+  /* gap: 30px; */
+`;
+export const JokeFactAnswer = styled.p`
+  font-size: 40px;
+  /* font-weight: bold; */
+  color: black;
+`;
